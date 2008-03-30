63748c89a3b37672ef012d28a69ba3627ed0f2ae
humitos <humitos@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-03-30 18:51:40 +0000

    Piezas estaticas

--- lib/pieces.py
+++ lib/pieces.py
@@ -5,33 +5,36 @@ import os
 import glob
 import utils
 from levelposimages import *
-#from pygame.locals import *
-#from config import *
 import math
 import random
 
 class Piece(pygame.sprite.Sprite):
     functions = [lambda x: 20*math.sin(x/4),
                  lambda x: 20*math.cos(x/2)]
-    def __init__(self, id, img):
+    def __init__(self, id, img, static=False):
         self.id = id
         self.selected = False
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.rect = self.image.get_rect()
-        self.rect.topleft = level1[id]
-        self.min_vel, self.max_vel = 0,0#self._velocity()
-        self.num = self.count(self.min_vel, self.max_vel)
-        self.func_x = random.choice(self.functions)
-        cordenates = range(HEIGHT) # alto
-        self.x = random.choice(cordenates)
-        self.y = random.choice(cordenates)
+        self.static = static
+        if self.static:
+            self.rect.topleft = level1[id]
+        else:
+            self.min_vel, self.max_vel = 0,0#self._velocity()
+            self.num = self.count(self.min_vel, self.max_vel)
+            self.func_x = random.choice(self.functions)
+            cordenates = range(HEIGHT) # alto
+            self.x = random.choice(cordenates)
+            self.y = random.choice(cordenates)
 
     def _velocity(self):
         largo, ancho = self.rect.size
         return (largo * ancho) / float(500) + 2
 
     def update(self):
+        if self.static:
+            return
         if self.selected:
             self.rect.center = pygame.mouse.get_pos()
 
@@ -56,8 +59,9 @@ class Piece(pygame.sprite.Sprite):
             yield x
 
 class Pieces():
-    def __init__(self):
+    def __init__(self, static=False):
         self.images = self.load_images()
+        self.static = static
 
     def load_images(self):
         result = []
@@ -71,7 +75,7 @@ class Pieces():
     def get_all(self):
         result = []
         for id, img in enumerate(self.images):
-            piece = Piece(id+1, img)
+            piece = Piece(id+1, img, self.static)
             result.append(piece)
         return result
 
