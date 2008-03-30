54c17a02dea1918affb23b1ad22b48041e91f3a3
jjconti <jjconti@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-03-30 15:13:28 +0000

    Esqueleto de main.

--- trunk/lib/config.py
+++ trunk/lib/config.py
@@ -3,10 +3,12 @@ import os
 # configuraciones
 
 # pantalla
-WINDOW_TITLE = "Robot!"
+WINDOW_TITLE = "Ojalata!"
 WIDTH = 780
 HEIGHT = 560
 CENTER = HEIGHT / 2
 
+CLOCK_TICS = 100
+
 # todos los path
-PIECES_PATH = os.path.join()
+#PIECES_PATH = os.path.join()
--- trunk/lib/main.py
+++ trunk/lib/main.py
@@ -1,12 +1,46 @@
-'''Game main module.
+import sys
+import pygame
+from pygame.locals import *
+from config import  *
+from level import Level
+#from menu import Menu
+#from scores import HighScores
+#from visual import Visual
+#from help import Help
+#from credits import Credits
 
-Contains the entry point used by the run_game.py script.
 
-Feel free to put all your game code here, or in other modules in this "lib"
-directory.
-'''
-import data
+if not pygame.font: print 'Warning, fonts disabled'
+if not pygame.mixer: print 'Warning, sound disabled'
 
 def main():
-    print "Hello from your game's main()"
-    print data.load('sample.txt').read()
+    #Initialize 
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption(WINDOW_TITLE)
+    #icon = utils.load_image(ICON, (0,0,0))
+    #pygame.display.set_icon(icon)
+
+    #Introduction
+    #images = [utils.load_image(image) for image in INTRO_IMAGES]
+    #visual = Visual(screen, images, INTRO_TIMES)
+    #music.play_music(INTROMUSIC, 1)
+    #visual.loop()
+    #music.stop_music()
+    
+    #Shooter opcion
+    #opcion = menu
+    #while opcion is not exit:
+    #    change = opcion(screen).loop()
+    #    if change:
+    #        opcion = change
+    #opcion()        #Exit
+
+    level = Level(screen, None) 
+    level.loop()
+
+def exit():
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
