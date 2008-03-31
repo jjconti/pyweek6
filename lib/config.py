024179397be5bf4e2ff0ab9c8b208c075a6fca81
gheize <gheize@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-03-31 13:43:40 +0000

    Agregado de la carpeta /fonts

--- lib/config.py
+++ lib/config.py
@@ -13,7 +13,13 @@ CLOCK_TICS = 100
 # todos los path
 DATA = os.path.join("data")
 IMGS = os.path.join(DATA, "imgs")
+FONTS = os.path.join(DATA, "fonts")
+CREDITS = os.path.join(DATA, "credits.txt")
 
 # imagenes
 LEVEL1 = os.path.join(IMGS, "level1")
 PIECES_LEVEL1 = os.path.join(LEVEL1, "pieces")
+
+# Font
+FONT_CREDIT = os.path.join(FONTS, "GALACTOS.ttf")
+
