f3c6eede7e2b2318be6ef5672063932b0a766fa5
gheize <gheize@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-04-02 03:57:47 +0000

    Avanzando sobre los creditos

--- lib/credits.py
+++ lib/credits.py
@@ -13,6 +13,7 @@ import sys
 ##
 # Módulos propios
 from config import FONT_CREDIT, CREDITS, WINDOW_TITLE,IMAGE_CREDITS
+from config import GREY
 
 
 class Credits(object):
@@ -20,9 +21,10 @@ class Credits(object):
         self.screen = screen
         self.father = father
         self.developers = []
-        self.font_1 = pygame.font.Font(FONT_CREDIT, 30)
-        self.font_2 = pygame.font.Font(FONT_CREDIT, 40)
-        self.font_3 = pygame.font.Font(FONT_CREDIT, 60)
+        self.font = []
+        for x in xrange(2,5):
+            print x*10
+            self.font.append(pygame.font.Font(FONT_CREDIT, x*10))
 
     def loop(self):
         pygame.event.clear()
@@ -36,66 +38,77 @@ class Credits(object):
             credits = csv.reader(open(CREDITS))
             for credit in credits:
                 self.developers.append(credit)
-            self._developers()
         except IOError:
             print 'Cannot open credits file'
+
+    def _generar_imagenes(self, developer):
+        """Genero por cada tamaño de letra una cantidad de palabras para ir
+        mostrandolas en el mismo tiempo en distintas coordenadas"""
+        lista = {}
+        for pos, font in enumerate(self.font):
+            lista[pos] = []
+            for obj in range(3):
+                lista[pos].append(font.render(developer, True, GREY))
+        return lista
+
+    def _dibujar_secuencia(self, lista_imagenes, background, topleft):
+        """Permitirá mostrar las letras en la secuencia de descenso"""
+        pos_x_inicial = 348
+        pos_y = y = topleft[1] + 50
+        bandera = False
+        clock = pygame.time.Clock()
+        time_loop = 0
+        while not bandera:
+            clock.tick(1000)
+            pygame.display.flip()
+            self.screen.blit(background, (0,0))
+            time_loop += 1
+            if time_loop == 1000:
+                bandera = True
+            if self._verifyKey():
+                return self.father #FIXME
+            if y >= 160:
+                # Poner imagen_2
+                for pos, font in enumerate(lista_imagenes[1]):
+                    self.screen.blit(lista_imagenes[1][pos],
+                            (pos_x_inicial, y))
+                #pygame.time.delay(50)
+                #pass
+                y += 2
+            elif y >= 190:
+                # Poner imagen_3
+                for pos, font in enumerate(lista_imagenes[2]):
+                    self.screen.blit(lista_imagenes[2],
+                            (pos_x_inicial, y))
+                y +=2
+                #pygame.time.delay(100)
+            else:
+                # Palabras pequeñas
+                for pos, font in enumerate(lista_imagenes[0]):
+                    self.screen.blit(lista_imagenes[0][pos],
+                            (pos_x_inicial, y))
+                y += 2
 
     def _draw_screen(self):
         pygame.display.set_caption(WINDOW_TITLE)
         image = IMAGE_CREDITS
         background = pygame.image.load(image)
 
-        #title = 'CREDITS'
-        #title_img = self.font_3.render(title, True, (100, 100, 100))
-        #topleft = (background.get_rect().width - title_img.get_rect().width) / 2, 30
-        #background.blit(title_img, topleft)
-        #self.screen.blit(background, (0,0))
-        #pygame.display.flip()
-        clock = pygame.time.Clock()
+        title = 'CREDITS'
+        title_img = self.font[-1].render(title, True, (100, 100, 100))
+        topleft = (background.get_rect().width - title_img.get_rect().width) / 2, 30
+        background.blit(title_img, topleft)
+        self.screen.blit(background, (0,0))
+        pygame.display.flip()
 
         # Probemos con dos nomas
         lista_aux = self.developers[1:4]
         while True:
             for developer in lista_aux:#self.developers[0:2]:
-                print "Viene ", developer
-                bandera = False
-                time_loop = 0
-                pos_x_inicial = 250
-                pos_y = y = 100#background.get_rect().width/2
                 developer = ' '.join(developer)
                 # genero las imagenes para mostrar
-                imagen_1 = self.font_1.render(developer, True,
-                        (100,100,100))
-                imagen_2 = self.font_2.render(developer, True,
-                        (100,100,100))
-                imagen_3 = self.font_3.render(developer, True,
-                        (100,100,100))
-
-                while not bandera:
-                    clock.tick(60)
-                    pygame.display.flip()
-                    self.screen.blit(background, (0,0))
-                    time_loop += 1
-                    if time_loop == 60:
-                        bandera = True
-                    if self._verifyKey():
-                        return self.father
-                    print 'Posicion Y: ', y
-                    if y >= 190:
-                        # Poner imagen_2
-                        self.screen.blit(imagen_2, (pos_x_inicial, y))
-                        pygame.time.delay(50)
-                        #pass
-                    elif y >= 220:
-                        # Poner imagen_3
-                        self.screen.blit(imagen_3, (pos_x_inicial, y))
-                        pygame.time.delay(100)
-                        #pass
-                    else:
-                        self.screen.blit(imagen_1, (pos_x_inicial, y)) # dibuja la imagen
-                    y += 5
-
-            pygame.time.delay(100)
+                lista_imagenes = self._generar_imagenes(developer)
+                self._dibujar_secuencia(lista_imagenes, background, topleft)
 
     def _developers(self):
         print self.developers
