ec13f052123d9ea70718edbfeb9a3a77048ce9ec
gheize <gheize@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-04-04 22:40:49 +0000

    cambio de alpha en el robot principal en los distintos niveles

--- lib/config.py
+++ lib/config.py
@@ -17,6 +17,10 @@ MINI_ROBOT_OFFSET = (WIDTH - 170, 0)
 #BACKGROUND_OFFSET = (270,20)
 BAR_OFFSET = (WIDTH - 30, 220)
 
+ALPHA_ROBOT = {1: 50,
+               2: 50,
+               3: 150}
+
 # todos los path
 DATA = os.path.join("data")
 IMGS = os.path.join(DATA, "imgs")
