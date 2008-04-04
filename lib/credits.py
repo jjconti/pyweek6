a8cafa533a2ac6a698eef84397c41f59052a24c8
gheize <gheize@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-04-04 04:12:15 +0000

    Modificaciones credits y config

--- lib/credits.py
+++ lib/credits.py
@@ -18,24 +18,16 @@ from config import *
 
 class Credits(object):
     def __init__(self, screen, father=None):
-        self.flag = True
         self.screen = screen
         self.father = father
-        self.font = self._generate_fontSize()
-        self.developers = [self._generate_images(name) for name in self._load_credits()]
+        self.developers = []
+        self.font = []
+        self.font = self.generar_fuentes()
 
-    def loop(self):
-        pygame.event.clear()
-        while self.flag:
-            if self._verifyKey():
-                self.flag = False
-            self._draw_screen()
-        return self.father
-
-    def _generate_fontSize(self):
+    def generar_fuentes(self):
         """Crea la lista de tamaños de fuentes y la fuente en sí"""
         #base = 10
-        sizes = [16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        sizes = [25, 22, 20, 18, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         lista_size = []
         for num in range(16):
             lista_size.append(num*sizes[num])
@@ -51,48 +43,70 @@ class Credits(object):
         lista_font = []
         # Gereramos las fuentes
         for size in lista_size:
-            lista_font.append(pygame.font.Font(FONT_CREDITS, size))
+            lista_font.append(pygame.font.Font(None, size))
         return lista_font
 
+    def loop(self):
+        pygame.event.clear()
+        self._load_credits()
+        self._draw_screen()
+
     def _load_credits(self):
-        list_name = []
+        """Carga el archivo credits.txt. Con los nombres hace un shuffle para
+        que cada vez que se muestre esta seccion sea distinto el orden de
+        aparición."""
         try:
             credits = csv.reader(open(CREDITS))
-            list_aux = []
+            lista_nombre = []
             for pos, credit in enumerate(credits):
+                print pos, credit
                 if 0 < pos < 8 and len(credit) == 2:
-                    list_aux.append(credit)
+                    lista_nombre.append(credit)
                 elif pos == 8:
-                    random.shuffle(list_aux)
-                    for gente in list_aux:
-                        list_name.append(gente)
-                    list_name.append(credit)
+                    random.shuffle(lista_nombre)
+                    for gente in lista_nombre:
+                        self.developers.append(gente)
+                    self.developers.append(credit)
                 else:
-                    list_name.append(credit)
-            list_aux = list_name
-            list_name = []
-            for developer in list_aux:
-                developer = ' '.join(developer)
-                list_name.append(developer)
+                    self.developers.append(credit)
         except IOError:
             print 'Cannot open credits file'
-        return list_name
 
-    def _generate_images(self, developer):
-        list_name = []
+    def _generar_imagenes(self, developer):
+        """Genero por cada tamaño de letra una cantidad de palabras para ir
+        mostrandolas en el mismo tiempo en distintas coordenadas"""
+        lista = []
         for font in self.font:
-            list_name.append(font.render(developer, True, GREY))
-        return list_name
+            lista.append(font.render(developer, True, GREY))
+        return lista
 
-    def _verifyKey(self):
-        if pygame.event.peek([KEYDOWN, KEYUP, QUIT]):
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    sys.exit(0)
-                if event.type == KEYDOWN and \
-                    (event.key in [K_ESCAPE, K_RETURN, K_KP_ENTER]):
-                    return True
-        return False
+    def _dibujar_secuencia(self, lista_imagenes, background, topleft):
+        """Permitirá mostrar las letras en la secuencia de descenso"""
+        y = (WIDTH / 2) - 300 
+        print y
+        bandera = False
+        cambiar = False
+        clock = pygame.time.Clock()
+        time_loop = 0
+        while not bandera:
+            clock.tick(CLOCK_TICS)
+            pygame.display.flip()
+            self.screen.blit(background, (0,0))
+            time_loop+=1
+            if cambiar or time_loop==1000:#y >= 780 or time_loop == 1000:
+                bandera = True
+            else:
+                if self._verifyKey():#FIXME
+                    return self.father
+                for pos, font in enumerate(lista_imagenes):
+                    self.screen.blit(background, (0,0))
+                    pos_x_inicial = (WIDTH / 2 - font.get_width() / 2)
+                    self.screen.blit(font, (pos_x_inicial, y))
+                    pygame.time.delay(80)
+                    y += font.get_height() - 50
+                    pygame.display.flip()
+                    cambiar = True
+                pygame.time.delay(1000)
 
     def _draw_screen(self):
         pygame.display.set_caption(WINDOW_TITLE)
@@ -105,51 +119,28 @@ class Credits(object):
         background.blit(title_img, topleft)
         self.screen.blit(background, (0,0))
         pygame.display.flip()
-        while self.flag:
-            for name in self.developers:
-                self._dibujar_secuencia(name, background, topleft)
-                if self._verifyKey():
-                    self.flag = False
-                    break
 
-# ****************************************************************************************************
+        # Probemos con dos nomas
+        lista_aux = self.developers
+        while True:
+            for developer in lista_aux:
+                developer = ' '.join(developer)
+                # genero las imagenes para mostrar
+                lista_imagenes = self._generar_imagenes(developer)
+                self._dibujar_secuencia(lista_imagenes, background, topleft)
 
-    def _dibujar_secuencia(self, lista_imagenes, background, topleft):
-        """Permitirá mostrar las letras en la secuencia de descenso"""
-        y = (WIDTH / 2) - 300 
-        print y
-        bandera = False
-        cambiar = False
-        clock = pygame.time.Clock()
-        time_loop = 0
-        while not bandera:
-            clock.tick(CLOCK_TICS)
-            if self._verifyKey():#FIXME
-                self.flag = False
-                return
-            pygame.display.flip()
-            self.screen.blit(background, (0,0))
-            time_loop+=1
-            if cambiar or time_loop==1000:#y >= 780 or time_loop == 1000:
-                bandera = True
-            else:
-                if self._verifyKey():#FIXME
-                    self.flag = False
-                    return
-                for pos, font in enumerate(lista_imagenes):
-                    if self._verifyKey():
-                        self.flag = False
-                        return
-                    self.screen.blit(background, (0,0))
-                    pos_x_inicial = (WIDTH / 2 - font.get_width() / 2)
-                    self.screen.blit(font, (pos_x_inicial, y))
-                    pygame.time.delay(80)
-                    y += font.get_height() - 50
-                    pygame.display.flip()
-                    cambiar = True
-                pygame.time.delay(1000)
+    def _developers(self):
+        print self.developers
 
-# ****************************************************************************************************
+    def _verifyKey(self):
+        if pygame.event.peek([KEYDOWN, KEYUP, QUIT]):
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    sys.exit(0)
+                if event.type == KEYDOWN and \
+                    (event.key in [K_ESCAPE, K_RETURN, K_KP_ENTER]):
+                    return True
+        return False
 
 if __name__ == '__main__':
     pygame.init()
