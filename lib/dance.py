9ed650d868f423ab2d5ee8b5e31f20028b39ef86
jjconti <jjconti@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-04-04 23:40:24 +0000

    Funciones para music y arreglos en el HappyDance.

--- lib/dance.py
+++ lib/dance.py
@@ -6,9 +6,9 @@ class HappyDance(object):
     def __init__(self, screen, menu):
         images = [utils.load_image_alpha(image) for image in DANCE_IMAGE]
         times = [0.1] * len(DANCE_IMAGE)
-        self.visual = Visual(screen, images, times, None, loopear=False)
+        self.visual = Visual(screen, images, times, menu, loopear=False)
         #return visual
         #return menu
 
     def loop(self):
-        self.visual.loop()
+        return self.visual.loop()
