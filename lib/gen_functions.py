f538eee805355986d4aa6e4515d10bf1f8296bf0
chinomng <chinomng@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-04-05 07:29:28 +0000

    funciones de descenso mejorada

--- lib/gen_functions.py
+++ lib/gen_functions.py
@@ -1,17 +1,33 @@
 import math
 import string
+import random
 
-funciones = [lambda x,direccion: direccion*20*math.sin(x/2),
-                 lambda x,direccion: direccion*20*math.cos(x/2),
-                 lambda x,direccion: direccion*x,
-                 lambda x,direccion: direccion*x**2/6,
-                 lambda x,direccion: -direccion*x**2/6,
-                 #lambda x,d: math.log(x)*x+math.sin(x),
-                 lambda x,d: (x**2*math.cos(x)/5) / 15,
-                 lambda x,d: math.sin(x) * 25]
+#funciones = [    lambda x,direccion: direccion*20*math.sin(x/2),
+                 #lambda x,direccion: direccion*20*math.cos(x/2),
+                 #lambda x,direccion: direccion*x,
+                 #lambda x,direccion: direccion*x**2/6,
+                 #lambda x,direccion: -direccion*x**2/6,
+                 ##lambda x,d: math.log(x)*x+math.sin(x),
+                 #lambda x,d: (x**2*math.cos(x)/5) / 15,
+                 #lambda x,d: math.sin(x) * 25]
+
+
+
+
+funciones = [
+    lambda x,d: math.sin(x*.05) * 60,
+    lambda x,direccion: direccion*1.5*x,
+    lambda x,direccion: direccion*12*x**2/200,
+    lambda x,direccion: -1*12*x**2/200,
+    lambda x,d: x*3*math.sin(x*.05),
+]
+
+
 
 for n, f in zip(string.letters, funciones):
     print '%s = {' % n,
     for x in xrange(6000):
         print "%i:%f," % (x,f(math.radians(x)*50,1))
     print '}'
+
+print "functions=[" + ", ".join(["%s" % x for x in string.letters[:len(funciones)]]) + "]"
