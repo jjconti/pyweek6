d22cf031fceeed96f186ecd9cf363d20b4b7c9ca
gheize <gheize@5082f4b2-5b49-0410-981d-7deb1f891687>
2008-04-05 03:08:41 +0000

    aumento el dance

--- trunk/lib/dance.py
+++ trunk/lib/dance.py
@@ -5,7 +5,7 @@ import utils
 class HappyDance(object):
     def __init__(self, screen, menu):
         images = [utils.load_image_alpha(image) for image in DANCE_IMAGE]
-        times = [0.1] * len(DANCE_IMAGE)
+        times = [0.05] * len(DANCE_IMAGE)
         self.visual = Visual(screen, images, times, menu, loopear=False)
         #return visual
         #return menu
